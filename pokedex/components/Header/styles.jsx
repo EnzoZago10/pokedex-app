d9d148c559ede34ff99d6b73e7bfb822e2305535
EnzoZago10 <enzozago2010@gmail.com>
2026-06-09 12:27:47 -0300

    Criando o projeto

---
+++ pokedex/components/Header/styles.jsx
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        marginBottom: 10,
+    },
+    image: {
+        width: "100%",
+        opacity: 0.2,
+        height: 250,
+        position: "absolute",
+        top: -150,
+        resizeMode: "cover",
+    },
+    titleContainer: {
+        paddingHorizontal: 15,
+        marginTop: -40,
+    },
+    backButton: {
+        marginRight: 15,
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: "bold",
+    },
+});
